e998781a07a52ff004474ac1fa0bd4c0a09e6419
Andrey Strizhkin <astrizhkin@biarum.com>
2024-09-28 17:16:07 +0300

    local assets

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,6 +3,11 @@
 rm -rf ./build
 flutter clean
 flutter build web --web-renderer canvaskit --release
+#--dart-define=FLUTTER_WEB_CANVASKIT_URL=./canvaskit.js 
+#--dart-define=FLUTTER_WEB_USE_SKIA=true
+#--no-web-resources-cdn
+#--base-href "/" 
+#flutter build web --web-renderer canvaskit --no-web-resources-cdn --release
 
 rm -rf ../open_mower_ros/web
 
