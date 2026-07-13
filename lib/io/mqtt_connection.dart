4819af29b5d03e3b0c2900772e5b62b945c1854b
astrizhkin <astrizhkin@biarum.com>
2026-07-13 13:35:09 +0300

    revert keepalive 1

--- lib/io/mqtt_connection.dart
+++ lib/io/mqtt_connection.dart
@@ -57,7 +57,7 @@ class MqttConnection  {
 
   void start() {
     // client.logging(on: true);
-    client.keepAlivePeriod = 20;
+    client.keepAlivePeriod = 1;
     client.autoReconnect = false;
     client.resubscribeOnAutoReconnect = false;
     client.onConnected = onConnected;
